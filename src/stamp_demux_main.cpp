552f76800e4c72b4c015763ba36a0590f45fd5b2
IliTheButterfly <iliana.decarufel@gmail.com>
2026-04-07 12:01:02 -0400

    Removed auto declare params

--- src/stamp_demux_main.cpp
+++ src/stamp_demux_main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
 
   rclcpp::NodeOptions options;
-  options.automatically_declare_parameters_from_overrides(true);
 
   auto node = std::make_shared<StampDemuxNode>(options);
   rclcpp::spin(node);
